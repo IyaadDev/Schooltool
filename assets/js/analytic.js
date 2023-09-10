8d52f3212e9602a2f6e6e47fe62529c3937e078c
IyaadDev <113124292+IyaadDev@users.noreply.github.com>
2023-09-10 12:14:09 +0400

    Update A

---
+++ assets/js/analytic.js
@@ -0,0 +1,5 @@
+  (function() { window.satismeter = window.satismeter || function() {(window.satismeter.q = window.satismeter.q || []).push(arguments);};window.satismeter.l = 1 * new Date();var script = document.createElement("script");var parent = document.getElementsByTagName("script")[0].parentNode;script.async = 1;script.src = "https://app.satismeter.com/js";parent.appendChild(script);})();
+
+  satismeter({
+    writeKey: "WKZHgng9WO3fupbo1KEabgdV3gleyiCz"
+  });
