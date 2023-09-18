ff83c51027590adb389f61713969439e11a56174
IyaadDev <113124292+IyaadDev@users.noreply.github.com>
2023-09-18 15:39:53 +0400

    ATTEMPT A - BACKGROUND JS

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,29 +1,27 @@
+// background.js
+
 // Function to get the value of a cookie by its name
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-// Function to redirect to the specified URL
-function redirectTo(url) {
-  window.location.href = url;
-}
-
-// Main function to check and handle the cookie
-function checkBackgroundCookie() {
-  const backgroundImgCookie = getCookie("backgroundImg");
-  const currentURL = encodeURIComponent(window.location.href);
-  const customizePageURL = "https://schooltool.io/app/customize-A.html";
-
-  if (backgroundImgCookie) {
-    // If the cookie exists, set the background image
-    const imageUrl = decodeURIComponent(backgroundImgCookie);
-    setBackground(imageUrl);
-  } else if (currentURL !== customizePageURL) {
-      console.log("Uncustomized Background")
+function getCookie(cookieName) {
+  const cookies = document.cookie.split('; ');
+  for (const cookie of cookies) {
+    const [name, value] = cookie.split('=');
+    if (name === cookieName) {
+      return decodeURIComponent(value);
+    }
   }
+  return null; // Cookie not found
 }
 
-// Call the main function when the script is executed
-checkBackgroundCookie();
+// Check if the "backgroundImg" cookie exists
+const backgroundCookie = getCookie("backgroundImg");
+
+if (backgroundCookie) {
+  // Log the contents of the "backgroundImg" cookie to the console
+  console.log("Contents of 'backgroundImg' cookie:", backgroundCookie);
+
+  // Set the background image for the body of the webpage using CSS
+  document.body.style.backgroundImage = `url('${backgroundCookie}') !important`;
+  document.body.style.backgroundSize = "cover"; // Optional: Set background size
+} else {
+  console.log("'backgroundImg' cookie does not exist.");
+}
