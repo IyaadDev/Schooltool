4f5e692520d6d1c948ec67f15f34ba90913677c6
IyaadDev <113124292+IyaadDev@users.noreply.github.com>
2023-09-19 10:27:31 +0400

    Added customize B, updated backgroundJS

--- assets/js/background.js
+++ assets/js/background.js
@@ -12,16 +12,21 @@ function getCookie(cookieName) {
   return null; // Cookie not found
 }
 
-// Check if the "backgroundImg" cookie exists
-const backgroundCookie = getCookie("backgroundImg");
+// Check if the current page is within the /app folder
+if (window.location.pathname.startsWith('/app')) {
+  // Check if the "backgroundImg" cookie exists
+  const backgroundCookie = getCookie("backgroundImg");
 
-if (backgroundCookie) {
-  console.log("Found 'backgroundImg' cookie with value:", backgroundCookie);
-  
-  // Set the image from the "backgroundImg" cookie as the background of the webpage
-  document.body.style.backgroundImage = `url(${backgroundCookie})`;
-  
-  console.log("Background image set successfully!");
+  if (backgroundCookie) {
+    console.log("Found 'backgroundImg' cookie with value:", backgroundCookie);
+    
+    // Set the image from the "backgroundImg" cookie as the background of the webpage
+    document.body.style.backgroundImage = `url(${backgroundCookie})`;
+    
+    console.log("Background image set successfully!");
+  } else {
+    console.log("'backgroundImg' cookie does not exist.");
+  }
 } else {
-  console.log("'backgroundImg' cookie does not exist.");
+  console.log("This script is only intended for pages within the /app folder.");
 }
